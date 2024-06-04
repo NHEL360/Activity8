52c72bcd34c38b0418dc6e373f0e18324b80dec5
NHEL360 <nhelmeynard@gmail.com>
2024-06-04 09:57:55 +0800

    modified config from database

--- it28-admin/db/config.php
+++ it28-admin/db/config.php
@@ -1,17 +1,18 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-
+/*
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'licera');
-/*
+*/
+
 define('DB_SERVER', 'localhost');
 define('DB_NAME', 'u593341949_db_licera');
 define('DB_USERNAME', 'u593341949_dev_licera');
 define('DB_PASSWORD', '20201362Licera'); 
-*/
+
 
 /* Attempt to connect to MySQL database */
 
