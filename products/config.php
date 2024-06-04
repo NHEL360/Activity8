5f851d18ac788a6ed2e57337ea63f5ab1d8d3849
NHEL360 <nhelmeynard@gmail.com>
2024-06-04 09:55:50 +0800

    Modified config product

--- products/config.php
+++ products/config.php
@@ -1,30 +1,30 @@
 <?php
-
+/*
 $host = 'localhost';
 $dbname = 'licera';
 $username = 'root';
 $password = '';
+*/
 
-/*
 define('DB_SERVER', 'localhost');
 define('DB_NAME', 'u593341949_db_licera');
 define('DB_USERNAME', 'u593341949_dev_licera');
 define('DB_PASSWORD', '20201362Licera');
-*/
 
+/*
 try {
 $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
 die("Database connection failed: " . $e->getMessage());
-}
+}*/
 
-/*
+
 try{
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
     die("ERROR: Could not connect. " . $e->getMessage());
-}*/
+}
 ?>
