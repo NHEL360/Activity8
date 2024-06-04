636044da4943dbca82910692673a321f5131dfeb
NHEL360 <nhelmeynard@gmail.com>
2024-06-04 10:01:26 +0800

    modified address config.php

--- it28-admin/address/address_config.php
+++ it28-admin/address/address_config.php
@@ -1,17 +1,17 @@
 <?php
 // Database configuration
-
+/*
 $dbHost = 'localhost';
 $dbName = 'licera';
 $dbUsername = 'root';
 $dbPassword = '';
+*/
 
-/*
 $host = 'localhost';
 $dbname = 'u593341949_db_licera';
 $username = 'u593341949_dev_licera';
 $password = '20201362Licera';
-*/
+
 
 try {
     $pdo = new PDO("mysql:host={$dbHost};dbname={$dbName}", $dbUsername, $dbPassword);
